774e394f68226a57185352e7c7d66e66f9dcc7dd
christianhall058 <christianhall058@gmail.com>
2023-10-17 10:39:43 -0500

    sending it home bois

---
+++ JokeWritingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeWritingApp.Models;
+
+namespace JokeWritingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeWritingApp.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ JokeWritingApp/Models/Joke.cs
@@ -0,0 +1,12 @@
+﻿namespace JokeWritingApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke() {
+            
+        }
+    }
+}
